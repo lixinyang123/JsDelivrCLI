096b6039925cf832240c8aecb6ee400eac2dfd7b
lixinyang123 <lixinyangemil@outlook.com>
2021-08-02 03:29:31 +0800

    add build script

--- build.ps1
+++ build.ps1
@@ -1,5 +1,10 @@
 dotnet restore
 
+echo "Publish windows version"
 dotnet publish -r win-x64
+
+echo "Publish linux version"
 dotnet publish -r linux-x64
+
+echo "Publish osx version"
 dotnet publish -r osx-x64
